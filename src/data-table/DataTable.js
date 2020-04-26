0f8f79bf035d07f8bb74828b1a4bbef6744b0019
Anjib Mulepati <mulepatianjib@johndeere.com>
2020-04-26 11:26:45 -0500

    Get page size working

--- src/data-table/DataTable.js
+++ src/data-table/DataTable.js
@@ -37,7 +37,7 @@ const DataTable = () => {
     );
 
     const handlePageSizeChange = (pageSize) => {
-        setPageSize(pageSize);
+        setPageSize(pageSize === 'All' ? data.length : pageSize);
     };
 
     const handlePaginationChange = (currentPage) => {
